c9f94f4f794ea7c6c66cb1dcad79e3838b6b6766
Maksim Yurkov <yurkov.maksim.sergeevich@gmail.com>
2019-01-03 23:34:07 +0300

    Update ht-item-editor.js

--- ht-item-editor.js
+++ ht-item-editor.js
@@ -1,6 +1,9 @@
 "use strict";
 import { LitElement, html } from "@polymer/lit-element";
 import "@polymer/paper-input/paper-input.js";
+import "@polymer/iron-iconset-svg";
+import "@polymer/iron-icon";
+import "@polymer/paper-tooltip";
 import "@polymer/paper-toggle-button/paper-toggle-button.js";
 import "@01ht/ht-wysiwyg";
 import "@01ht/ht-spinner";
@@ -16,6 +19,11 @@ import "./ht-item-editor-categories.js";
 import "./ht-item-editor-attributes.js";
 import "./ht-item-editor-tags.js";
 import "./cloudinary-widget.js";
+
+import {
+  // callTestHTTPFunction,
+  callFirebaseHTTPFunction
+} from "@01ht/ht-client-helper-functions";
 
 class HTItemEditor extends LitElement {
   render() {
@@ -45,6 +53,31 @@ class HTItemEditor extends LitElement {
 
         paper-input {
           max-width: 500px;
+          width: 100%;
+        }
+
+        #nameInURLContainer {
+          display:flex;
+          align-items:center;
+          position:relative;
+          max-width: 500px;
+          width: 100%;
+        }
+
+        #name-in-url {
+          margin-right: 32px;
+        }
+
+        .warning {
+          color: var(--accent-color);
+          position: absolute;
+          top: 28px;
+          height: 24px;
+          right:0;
+          bottom:0;
+          left:0;
+          display:flex;
+          justify-content:flex-end;
         }
 
         .toggle-container {
@@ -61,6 +94,13 @@ class HTItemEditor extends LitElement {
           display:none;
         }
       </style>
+      <iron-iconset-svg size="24" name="ht-item-editor">
+        <svg>
+          <defs>
+              <g id="warning"><path d="M1 21h22L12 2 1 21zm12-3h-2v-2h2v2zm0-4h-2v-4h2v4z"></path></g>   
+          </defs>
+        </svg>
+      </iron-iconset-svg>
       <ht-spinner page text=${loadingText} ?hidden=${!loading}></ht-spinner>
       <div id="container" ?hidden=${loading}>
         <h1 class="mdc-typography--headline5">${
@@ -73,7 +113,18 @@ class HTItemEditor extends LitElement {
             ""}>Отображать в каталоге</paper-toggle-button>
         </div>
         <paper-input id="name" label="Название" allowed-pattern="^[0-9a-zA-Zа-яА-Я ]" char-counter maxlength="50"></paper-input>
-        <paper-input id="name-in-url" label="Название в URL" placeholder="my-super-item-7" allowed-pattern="^[0-9a-z\-]+$" char-counter maxlength="100"></paper-input>
+        <div id="nameInURLContainer">
+          <div class="warning">
+              <iron-icon icon="ht-item-editor:warning"></iron-icon>
+              <paper-tooltip>
+              Изменение влечет за собой изменение всех ссылок в которых задействован данный параметр. Существующие ссылки в интернете с параметром, станут недоступными и будут выдавать ошибку 404. Поисковым системам потребуется время для повторного индексирования ссылок и размещения информации в поисковой выдаче. Соответственно частое изменение данного параметра крайне не рекомендуется.
+            </paper-tooltip>
+          </div>
+          <paper-input id="name-in-url" label="Название в URL" placeholder="my-super-element-7" allowed-pattern="^[0-9a-z\-]+$" auto-validate char-counter maxlength="100">
+            <div slot="prefix">/item/</div>
+            <div slot="suffix"></div>
+          </paper-input>
+        </div>
         <paper-input id="github" label="Ссылка на репозиторий" placeholder="author/repository">
             <div slot="prefix">https://github.com/</div>
         </paper-input>
@@ -172,6 +223,9 @@ class HTItemEditor extends LitElement {
       this.shadowRoot.querySelector("#published").checked = false;
       this.shadowRoot.querySelector("#name").value = "";
       this.shadowRoot.querySelector("#name-in-url").value = "";
+      this.shadowRoot.querySelector(
+        "#name-in-url [slot='suffix']"
+      ).innerHTML = ``;
       this.shadowRoot.querySelector("#github").value = "";
       this.shadowRoot.querySelector("#author").reset();
       this.shadowRoot.querySelector("#description").setDefaultData();
@@ -197,6 +251,9 @@ class HTItemEditor extends LitElement {
       this.shadowRoot.querySelector("#published").checked = itemData.published;
       this.shadowRoot.querySelector("#name").value = itemData.name;
       this.shadowRoot.querySelector("#name-in-url").value = itemData.nameInURL;
+      this.shadowRoot.querySelector(
+        "#name-in-url [slot='suffix']"
+      ).innerHTML = `/${itemData.itemNumber}`;
       this.shadowRoot.querySelector("#github").value = itemData.repositoryURL;
       this.shadowRoot.querySelector("#author").data = itemData.authorData;
       this.shadowRoot
@@ -224,15 +281,13 @@ class HTItemEditor extends LitElement {
       this.loading = true;
       this.loadingText = "Создание продукта";
       let item = {};
-      item.created = firebase.firestore.FieldValue.serverTimestamp();
-      item.updated = firebase.firestore.FieldValue.serverTimestamp();
       item.status = "moderation";
       item.statusText = "Рассматривается модератором";
       item.published = false;
       item.ownerId = firebase.auth().currentUser.uid;
-      item.name = this.shadowRoot.querySelector("#name").value || "";
+      item.name = this.shadowRoot.querySelector("#name").value || "No name";
       item.nameInURL =
-        this.shadowRoot.querySelector("#name-in-url").value || "";
+        this.shadowRoot.querySelector("#name-in-url").value || "no-name";
       item.repositoryURL = this.shadowRoot.querySelector("#github").value || "";
       item.description = this.shadowRoot
         .querySelector("#description")
@@ -250,17 +305,39 @@ class HTItemEditor extends LitElement {
       item.tags = this.shadowRoot.querySelector("#tags").selected;
       item.authorData = await this.shadowRoot.querySelector("#author").data;
       item.sales = 0;
-      await firebase
-        .firestore()
-        .collection("items")
-        .add(item);
-      this.dispatchEvent(
-        new CustomEvent("on-add", {
-          bubbles: true,
-          composed: true
-        })
-      );
+
+      let response = await callFirebaseHTTPFunction({
+        name: "httpsItemsCreateItemIndex",
+        authorization: true,
+        options: {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify({
+            itemData: item
+          })
+        }
+      });
       this.loading = false;
+      if (response.created === true) {
+        this.dispatchEvent(
+          new CustomEvent("on-add", {
+            bubbles: true,
+            composed: true
+          })
+        );
+      } else {
+        this.dispatchEvent(
+          new CustomEvent("show-toast", {
+            bubbles: true,
+            composed: true,
+            detail: {
+              text: `При создании элемента возникла ошибка: ${response.error}`
+            }
+          })
+        );
+      }
     } catch (err) {
       console.log("add: " + err.message);
     }
@@ -274,9 +351,9 @@ class HTItemEditor extends LitElement {
       let updates = {};
       updates.updated = firebase.firestore.FieldValue.serverTimestamp();
       updates.published = this.shadowRoot.querySelector("#published").checked;
-      updates.name = this.shadowRoot.querySelector("#name").value || "";
+      updates.name = this.shadowRoot.querySelector("#name").value || "No name";
       updates.nameInURL =
-        this.shadowRoot.querySelector("#name-in-url").value || "";
+        this.shadowRoot.querySelector("#name-in-url").value || "no-name";
       updates.repositoryURL =
         this.shadowRoot.querySelector("#github").value || "";
       updates.description = this.shadowRoot
@@ -311,7 +388,7 @@ class HTItemEditor extends LitElement {
       );
       this.loading = false;
     } catch (err) {
-      console.log("add: " + err.message);
+      console.log("save: " + err.message);
     }
   }
 
